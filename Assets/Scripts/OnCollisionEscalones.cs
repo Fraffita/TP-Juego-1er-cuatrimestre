a50f42877a62183e50520b63853766e58a1f55b3
Franco Raffaele <fran.raffa15@gmail.com>
2022-06-03 20:40:16 -0300

    Movimiento del personaje terminado

---
+++ Assets/Scripts/OnCollisionEscalones.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionEscalones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //MeshRenderer renderer = GetComponent<MeshRenderer>();
+        //renderer.material = new Material(Shader.Find("Verde"));
+        if (collision.gameObject.tag == "Untagged") {
+            gameObject.SetActive(false); 
+        }
+
+        else
+        {
+            Debug.Log("No pasa nada");
+        }
+        //renderer.material = new Material(Shader.Find("Rojo"));
+    }
+
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            
+            transform.Translate(0, 0, movementSpeed);
+        } 
+        if (Input.GetKey(KeyCode.S)){
+            
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            
+            transform.Translate(0, movementSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            
+            transform.Translate(0, -movementSpeed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Force);
+        }
+    }
+}
